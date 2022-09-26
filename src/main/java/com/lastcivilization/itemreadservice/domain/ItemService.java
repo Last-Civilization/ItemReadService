fa7a0cc9ffdb5ad0231072c77cc8eda00151dd48
mrrevo55 <kwolny31@gmail.com>
2022-09-26 21:42:48 +0200

    Refactored code

--- src/main/java/com/lastcivilization/itemreadservice/domain/ItemService.java
+++ src/main/java/com/lastcivilization/itemreadservice/domain/ItemService.java
@@ -3,10 +3,6 @@ package com.lastcivilization.itemreadservice.domain;
 import com.lastcivilization.itemreadservice.domain.exception.ItemNotFoundException;
 import com.lastcivilization.itemreadservice.domain.port.ItemRepository;
 import com.lastcivilization.itemreadservice.domain.view.ItemModel;
-
-import static com.lastcivilization.itemreadservice.domain.Mapper.toDomain;
-import static com.lastcivilization.itemreadservice.domain.Mapper.toModel;
-
 public class ItemService {
 
     private final ItemRepository itemRepository;
@@ -17,8 +13,8 @@ public class ItemService {
     
     public ItemModel getItemById(long id) {
         ItemModel itemModel = getItem(id);
-        Item item = toDomain(itemModel);
-        return toModel(item);
+        Item item = Mapper.toDomain(itemModel);
+        return Mapper.toModel(item);
     }
 
     private ItemModel getItem(long id) {
